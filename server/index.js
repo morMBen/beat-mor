8fb4bf6cd14c9c8156081273e9c53fdce6e13449
mor <mor6686@gmail.com>
2021-05-15 20:33:04 +0300

    beat-mor_1.9

--- server/index.js
+++ server/index.js
@@ -5,12 +5,12 @@ require('./db/mongoose');
 const route = require('./routers/Index.router')
 
 const cors = require('cors')
+require('dotenv').config()
 
 
+const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT || 5000;
-
-app.use(express.json({ limit: "50mb" }))
+app.use(express.json({ limit: "5mb" }))
 app.use(cors())
 
 
