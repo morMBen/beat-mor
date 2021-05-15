9399957e1f7848449bf1365a8efdb62f4e79b16f
mor <mor6686@gmail.com>
2021-05-15 23:04:33 +0300

    beat-mor_2.5

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import reportWebVitals from './reportWebVitals';
 import './style.css'
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +10,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+reportWebVitals();
