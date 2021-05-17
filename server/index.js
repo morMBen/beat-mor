aaad44bb4e6e6fe21b6eace061b6f08f6a9c7881
mor <mor6686@gmail.com>
2021-05-17 20:46:52 +0300

    beat-mor_3.0

--- server/index.js
+++ server/index.js
@@ -5,33 +5,26 @@ const app = express();
 require('./db/mongoose');
 const route = require('./routers/Index.router')
 
-const PORT = process.env.PORT || 8000;
+app.use(express.static(path.join(__dirname, './build')));
 
-require('dotenv').config()
-
-
-
-app.use(express.json({ limit: "5mb" }))
 app.use(cors())
+app.use(express.json({ limit: "5mb" }))
 app.use('/api', route);
 
-app.use((req, res) => {
-    res.status(404).send('Error: Something went wrong... try again')
-})
+// app.use((req, res) => {
+//     res.status(404).send('Error: Something went wrong... try again')
+// })
 
-
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 // const pubDir = path.join(__dirname, "build");
 // app.use(express.static(pubDir));
 // app.use("/", (req, res) => {
 //     res.sendFile(path.join(pubDir, "index.html"));
 // });
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log('listening to port ' + PORT)
