e616bde634fe048d4271b4414e5fa618123310c3
mor <mor6686@gmail.com>
2021-05-22 23:05:55 +0300

    beat-mor_4.7

--- server/index.js
+++ server/index.js
@@ -1,43 +1,43 @@
-// const express = require('express')
-// const cors = require('cors')
-// const path = require('path');
-// const app = express();
-// require('./db/mongoose');
-// const route = require('./routers/Index.router')
-
-// const pubDir = path.join(__dirname, './build')
-
-// app.use(express.static(pubDir));
-
-// app.use(cors())
-// app.use(express.json({ limit: "5mb" }))
-// app.use('/api', route);
-
-
-// app.use('/', (req, res) => {
-//     res.sendFile(path.join(pubDir, 'index.html'));
-// });
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//     console.log('listening to port ' + PORT)
-// })
-
-
-
+const express = require('express')
+const cors = require('cors')
+const path = require('path');
+const app = express();
 require('./db/mongoose');
 const route = require('./routers/Index.router')
 
-app.use(express.static(path.join(__dirname, './build')));
+const pubDir = path.join(__dirname, './build')
+
+app.use(express.static(pubDir));
 
 app.use(cors())
 app.use(express.json({ limit: "5mb" }))
 app.use('/api', route);
 
 
+app.use('/', (req, res) => {
+    res.sendFile(path.join(pubDir, 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log('listening to port ' + PORT)
 })
+
+
+
+// require('./db/mongoose');
+// const route = require('./routers/Index.router')
+
+// app.use(express.static(path.join(__dirname, './build')));
+
+// app.use(cors())
+// app.use(express.json({ limit: "5mb" }))
+// app.use('/api', route);
+
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//     console.log('listening to port ' + PORT)
+// })
