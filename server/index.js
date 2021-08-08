4db819b72d0985e2e5f02bbce8e88dcf59e50160
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2021-08-08 12:55:24 +0300

    testing deploy

--- server/index.js
+++ server/index.js
@@ -12,11 +12,17 @@ app.use(express.static(pubDir));
 app.use(cors())
 app.use(express.json({ limit: "5mb" }))
 app.use('/api', route);
-
-
-app.use('/', (req, res) => {
-    res.sendFile(path.join(pubDir, 'index.html'));
-});
+//
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+//
+// app.use('/', (req, res) => {
+//     res.sendFile(path.join(pubDir, 'index.html'));
+// });
 
 const PORT = process.env.PORT || 5000;
 
