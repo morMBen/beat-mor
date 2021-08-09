c77e54dbab6f32a44d4ae37a970ee6c9d47b36c5
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2021-08-09 11:41:05 +0300

    %npm_config_first%

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,54 +1,46 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import Api from '../../api/Api'
-import './home.css'
-import testImg from '../../img/openBackgound.jpg'
 import { Link } from "react-router-dom";
-// import Spinner from "../../components/spinner/Spinner";
 
-// import OpeningPage from '../openingPage/OpeningPage';
+import Api from '../../api/Api'
+
+import './home.css';
+import homePageSectionImage from '../../img/openBackgound.jpg';
+
 const Home = ({ setConsoleIsOpen, setCurrentCollection, consoleIsOpen }) => {
     // // Loding spinner bool
     // const [isLoading, setIsLoading] = useState(true);
 
+    // Sounds collection neme
     const [collectionsNames, setCollectionsNames] = useState(null)
 
     useEffect(() => {
-        const getCollectionsName = async () => {
-            const res = await Api.get('/sound-collection')
-            setCollectionsNames(res.data)
-        }
-        getCollectionsName()
+        // At the begining load get the collections name and save them in the state.
+        getCollectionsName();
+        // Check if the App.js consoleIsOpen state is true and if it dose set it to false.
         if (consoleIsOpen) {
             setConsoleIsOpen(false)
         }
     }, [consoleIsOpen, setConsoleIsOpen])
 
+    // Send get request and set the results in the collectionName state
+    const getCollectionsName = async () => {
+        const res = await Api.get('/sound-collection')
+        setCollectionsNames(res.data)
+    }
+
+    // Set the App.js state consoleIsOpen to true
     const openConsole = () => {
         setConsoleIsOpen(true)
-        // useEffect(() => {
-        //     collectionsNames && setIsLoading(false);
-        // }, [collectionsNames, setIsLoading])
-
     }
-    // useEffect(() => {
-    //     const getCollectionsName = async () => {
-    //         const res = await Api.get('/sound-collection')
-    //         setCollectionsNames(res.data)
-    //     }
-    //     getCollectionsName()
-    // }, [])
 
-    // const openConsole = () => {
-    //     setConsoleIsOpen(true)
-
-    // }
-
-
+    // Map over all the collections from the state collectionName and return section for each collection.
+    // @return - section for each collection
     const insetCollectionsNames = () => {
         return collectionsNames.map((e, i) => {
             return <section className='home-section' key={`collection${i}`}
                 onClick={() => {
+                    // Set the App.js currentCollection to the choosen collection id
                     setCurrentCollection(e._id)
                     openConsole()
                 }}
@@ -57,11 +49,11 @@ const Home = ({ setConsoleIsOpen, setCurrentCollection, consoleIsOpen }) => {
                     to={'/console'}
                 >
                     <figure className='home-figure-img'>
-                        <img src={testImg} alt='fasd' />
+                        <img src={homePageSectionImage} alt='homePageSectionImage' className='home-img' />
                     </figure>
                     <figure className='home-figure-text'>
                         <article className='home-article'>
-                            <span >{e.ownerName}</span>
+                            <span className='home-span'>{e.ownerName}</span>
                             <h3 className='home-h3'>{e.name}</h3>
                         </article>
                     </figure>
@@ -70,8 +62,9 @@ const Home = ({ setConsoleIsOpen, setCurrentCollection, consoleIsOpen }) => {
         })
     }
     return (
-        <div style={{ paddingTop: '5rem' }}>
+        <div>
             <div className='home-main'>
+                {/* Check if collectionName state is initilaized and if it dose invoke the insertCollectionsName func */}
                 {collectionsNames && insetCollectionsNames()}
             </div>
         </div>
