20abc2705db5effc1880ad232f180ddfca2aee5f
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2021-08-09 09:39:41 +0300

    %npm_config_first%

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,27 +8,33 @@ import StarterPlayModePage from '../pages/‏‏playModePage/StarterPlayModePage
 
 
 const App = () => {
+  //Cheack if the playing console is open.
+  const [consoleIsOpen, setConsoleIsOpen] = useState(false);
+  //Current collection selected.
+  const [currentCollection, setCurrentCollection] = useState(null);
 
+  //check if the user is loged.
+  const [isUserLogedIn, setIsUserLogedIn] = useState(false);
 
-  const [consoleIsOpen, setConsoleIsOpen] = useState(false)
-
-  const [currentCollection, setCurrentCollection] = useState(null)
-
-  const [isLoged, setIsLoged] = useState(false)
+  //set the user name.
   const [userName, setUserName] = useState(null)
   useEffect(() => {
-    if (localStorage.token) {
-      setIsLoged(true)
+    //if token is saved in the local storage:
+    // - set the state isLoged to true.
+    // - set the state userName to the name from local storage.
+    if (localStorage.token && !isUserLogedIn) {
+      setIsUserLogedIn(true)
       setUserName(localStorage.getItem('name'))
     }
-  }, [isLoged])
+  }, [isUserLogedIn])
 
   return (
     <>
       <>
-        {!isLoged && <OpeningPage setIsLoged={setIsLoged} />}
+        {/* { console.log(window.location.href)} */}
+        {!isUserLogedIn && <OpeningPage setIsUserLogedIn={setIsUserLogedIn} />}
         <BrowserRouter>
-          {isLoged && <>
+          {isUserLogedIn && <>
             {!consoleIsOpen && <Navbar userName={userName} />}
             <Route path="/" exact
               component={() =>
@@ -57,6 +63,7 @@ const App = () => {
                 consoleIsOpen={consoleIsOpen}
                 setConsoleIsOpen={setConsoleIsOpen}
                 currentCollection={currentCollection}
+                setcurrentCollection={setCurrentCollection}
               />
             }
             />
@@ -68,4 +75,75 @@ const App = () => {
 }
 
 export default App;
+
+// import { useEffect, useState } from 'react';
+// import { BrowserRouter, Route } from 'react-router-dom'
+// import Home from '../pages/home/Home'
+// import Navbar from '../components/navbar/Navbar'
+// import OpeningPage from '../pages/openingPage/OpeningPage'
+// import Create from '../pages/create/Create'
+// import StarterPlayModePage from '../pages/‏‏playModePage/StarterPlayModePage'
+
+
+// const App = () => {
+
+
+//   const [consoleIsOpen, setConsoleIsOpen] = useState(false)
+
+//   const [currentCollection, setCurrentCollection] = useState(null)
+
+//   const [isLoged, setIsLoged] = useState(false)
+//   const [userName, setUserName] = useState(null)
+//   useEffect(() => {
+//     if (localStorage.token) {
+//       setIsLoged(true)
+//       setUserName(localStorage.getItem('name'))
+//     }
+//   }, [isLoged])
+
+//   return (
+//     <>
+//       <>
+//         {!isLoged && <OpeningPage setIsLoged={setIsLoged} />}
+//         <BrowserRouter>
+//           {isLoged && <>
+//             {!consoleIsOpen && <Navbar userName={userName} />}
+//             <Route path="/" exact
+//               component={() =>
+//                 <Home
+//                   setConsoleIsOpen={setConsoleIsOpen}
+//                   consoleIsOpen={consoleIsOpen}
+//                   setCurrentCollection={setCurrentCollection}
+//                 />
+//               }
+//             >
+//             </Route>
+//             < Route path="/home" exact component={() =>
+//               <Home
+//                 setConsoleIsOpen={setConsoleIsOpen}
+//                 consoleIsOpen={consoleIsOpen}
+//                 setCurrentCollection={setCurrentCollection}
+//               />
+//             }
+//             />
+//             < Route path="/create" exact component={() =>
+//               <Create />
+//             }
+//             />
+//             < Route path="/console" exact component={() =>
+//               <StarterPlayModePage
+//                 consoleIsOpen={consoleIsOpen}
+//                 setConsoleIsOpen={setConsoleIsOpen}
+//                 currentCollection={currentCollection}
+//               />
+//             }
+//             />
+//           </>}
+//         </BrowserRouter>
+//       </>
+//     </>
+//   );
+// }
+
+// export default App;
 
